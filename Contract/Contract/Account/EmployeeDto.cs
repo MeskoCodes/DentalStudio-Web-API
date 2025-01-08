93584fee25870907e49bb88faf7641ca696eaef6
Mersudin Murić <100204090+MeskoCodes@users.noreply.github.com>
2025-01-08 21:56:42 +0100

    Rijeseni projektni .dll errori

--- Contract/Contract/Account/EmployeeDto.cs
+++ Contract/Contract/Account/EmployeeDto.cs
@@ -11,10 +11,10 @@ namespace Contract.Account
 {
     public class EmployeeDto
     {
-        public int EmployeeId { get; set; } // ID zaposlenog
-        public string FirstName { get; set; } = string.Empty; // Ime
-        public string? LastName { get; set; } // Prezime
-        public string Specialization { get; set; } = string.Empty; // Specijalizacija
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; } 
+        public string Specialization { get; set; } = string.Empty;
         public string? MobileNumber { get; set; } // Broj telefona
         public string? Email { get; set; } // Email
         public List<string> Roles { get; } = new List<string>(); // Uloge zaposlenog
